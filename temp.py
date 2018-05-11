41d5c0530d0f7407d1182be8d278a466575a5e1d
Kyubyong Park <longinglove@nate.com>
2018-05-11 14:12:44 +0900

    Add files via upload

--- temp.py
+++ temp.py
@@ -1,31 +1,83 @@
 import urllib.request
 import codecs
 import re
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize
 
+# step1. construct cmu lookup dictionary
 url = "http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b"
 
+cmu = dict()
 lines = urllib.request.urlopen(url).read().decode('utf-8', errors="replace").splitlines()
-
-word2prons = dict()
 for line in lines:
     if line.startswith(";;;"): continue # comments
     word, pron = line.split("  ")
-    word = re.sub("\(\d+\)", "", word) # e.g. PROJECT(1) -> PROJECT
-    pron = pron.replace(" ", ".") # e.g. P OW1 L -> P.OW1.L
-    if word in word2prons:
-        word2prons[word].append(pron)
+    cmu[word] = pron
+
+# step2. construct homograph dictionary
+f = 'homographs.en'
+homograph2features = dict()
+for line in codecs.open(f, 'r', 'utf8').read().splitlines():
+    if line.startswith("#"): continue # comment
+    # print(line.strip().split("|"))
+    headword, pron1, pron2, pos1 = line.strip().split("|")
+    homograph2features[headword] = (pron1, pron2, pos1)
+
+# step3. Load trained model
+def word2pron(word):
+    pron = ""
+    pref, suf = word, ""
+    while len(pref) > 0:
+        print(pref, suf)
+
+        if pref in cmu:
+            # print(pref)
+            pron += cmu[pref] + " "
+            pref, suf = suf, ""
+        else:
+            pref, suf = pref[:-1], pref[-1] + suf
+            if pref == "":
+                pron += suf[0] + " "
+                pref, suf = suf[1:], ""
+
+    pron = pron.strip()
+    if pron == "":
+        return word
     else:
-        word2prons[word] = [pron]
+        return pron
 
-# print(word2prons)
-
-homographs = [line.strip().split("/")[0].upper() for line in codecs.open('homographs.txt', 'r', 'utf8').read().splitlines() if len(line)>0]
-# print(homographs)
-
-for h in homographs:
-    if h in word2prons:
-        print(h, "|".join(word2prons[h]))
-
-
-
-
+word = "resistentialism".upper()
+out = word2pron(word)
+print(out)
+# def token2pron(token):
+#     word, pos = token
+#     word = word.upper()
+#
+#     if word in homograph2features: # Check homograph
+#         pron1, pron2, pos1 = homograph2features[word]
+#         if pos.startswith(pos1):
+#             pron = pron1
+#         else:
+#             pron = pron2
+#     elif word in word2pron: # CMU dict
+#         pron = word2pron(word)
+#
+#
+#
+#
+#
+# def g2p(text):
+#     tokens = word_tokenize(text)
+#     ret = []
+#     for token in tokens:
+#         pron = word2pron(token)
+#         ret.extend(pron.split())
+#         ret.append(" ")
+#     ret = ret[:-1]
+#
+#
+# AH0 F AO1 R D AH0 B AH0 L
+#
+# ["A", "I", ".", " ", "A"]
+#
+#
