153ca5a13c6cf5ef4421dd9b454d1f1205c10510
Erdi Tac <erditac@gmail.com>
2022-04-21 09:50:44 +0800

    Update expand.py
    Error when text has values like "$12,000". Fix by removing the comma

--- g2p_en/expand.py
+++ g2p_en/expand.py
@@ -34,7 +34,7 @@ def _expand_dollars(m):
     parts = match.split('.')
     if len(parts) > 2:
         return match + ' dollars'    # Unexpected format
-    dollars = int(parts[0]) if parts[0] else 0
+    dollars = int(re.sub(',', '', parts[0])) if parts[0] else 0
     cents = int(parts[1]) if len(parts) > 1 and parts[1] else 0
     if dollars and cents:
         dollar_unit = 'dollar' if dollars == 1 else 'dollars'
